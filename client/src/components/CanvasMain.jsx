04eb1c172b8cabe071aa52103c250631ec8fe068
shofi777 <soup.bee@gmail.com>
2022-12-15 16:11:12 +0900

    Add more solutions on CanvasMain
    Add solution on CanvasMain to send landmark or video to the server

--- client/src/components/CanvasMain.jsx
+++ client/src/components/CanvasMain.jsx
@@ -1,9 +1,11 @@
-import React, { useRef, useEffect, useContext } from "react";
+import React, { useRef, useEffect, useContext, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { SocketContext } from "../SocketContext";
 import axios from 'axios';
 import {Holistic} from '@mediapipe/holistic';
 import * as HOLISTIC from '@mediapipe/holistic';
+import { Grid, Typography, Paper} from '@material-ui/core';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const useStyles = makeStyles(() => ({
     canvas: {
@@ -13,20 +15,45 @@ const useStyles = makeStyles(() => ({
     },
 }));
 
+function encodeImageBitmapToBase64(imageBitmap) {
+  const canvas = document.createElement('canvas');
+  canvas.width = imageBitmap.width;
+  canvas.height = imageBitmap.height;
+
+  const ctx = canvas.getContext('2d');
+  ctx.drawImage(imageBitmap, 0, 0);
+
+  const dataURL = canvas.toDataURL('image/png');
+  return dataURL.replace(/^data:image\/(png|jpg);base64,/, '');
+}
+
+function formatYAxis(value) {
+  if(value === 0) return "Not Engage"
+  if(value === 0.25) return ""
+  if(value === 0.5) return "Normal"
+  if(value === 0.75) return ""
+  if(value === 1) return "Engage"
+  return value
+}
+
 const CanvasMine = (props) => {
     const { myVideo } = useContext(SocketContext);
+    const [response, setResponse] = useState(null);
+    const [chartData, setChartData] = useState(null);
     const classes = useStyles();
     const canvasRef = useRef();
     const canvasRefuser = useRef();
  
     // ======================Holistic Mediapipe===========================
     const connect = window.drawConnectors;
+
+    
     
     function onResults(results){
       const videoElement = document.getElementById(props.id);
       const videoWidth = videoElement.videoWidth;
       const videoHeight = videoElement.videoHeight;
-      console.log(results)
+      // console.log(results)
 
       // Set canvas width
       canvasRef.current.width = videoWidth;
@@ -59,16 +86,44 @@ const CanvasMine = (props) => {
       // Step 1 - Send to prediction function and get the label and prob ()
       // Sol1: Just JS (does not work)
       // Sol2: Send landmark to python and get the class and prob
-      axios.post(`http://localhost:5050/api`, {
-        landmark_from_js: results,
-      })
-      .then(res => {
-        const predict_from_py = res.data;
-        console.log(predict_from_py.class)
-        console.log(predict_from_py.prob)
-      })
+      // axios.post(`http://localhost:5050/api`, {
+      //   landmark_from_js: results,
+      // })
+      // .then(res => {
+      //   const predict_from_py = res.data;
+      //   console.log(predict_from_py.class)
+      //   console.log(predict_from_py.prob)
+      // })
       // Sol3: Send video instead landmark
-      // Step 2 - Write to CSV and draw a graph (optional)]
+      const currentTime = new Date();
+      const time = currentTime.getTime().toString()
+      if (time.charAt(time.toString().length - 1) == 1) {
+        const encodedImage = encodeImageBitmapToBase64(results.image);
+        axios.post('http://localhost:5050/api', { encodedImage }).then((response) => {
+          console.log(response.data);
+          setResponse(response.data);
+        });
+      }
+      // Step 2 - Write to CSV and draw a graph
+      const data = [
+        {
+          "name": "1",
+          "engagement": 0,
+        },
+        {
+          "name": "2",
+          "engagement": 1,
+        },
+        {
+          "name": "3",
+          "engagement": 0.5,
+        },
+        {
+          "name": "4",
+          "engagement": 0,
+        }
+      ]
+      setChartData(data)
     }
 
     useEffect(() => {
@@ -111,6 +166,23 @@ const CanvasMine = (props) => {
           <>
           {props.id === "myVideoId"}
             <canvas ref={canvasRef} className={classes.canvas} />
+            {response && (
+              <div>
+                <Typography variant="h5" gutterBottom>{response.class}</Typography>
+                <Typography variant="h5" gutterBottom>{response.prob}</Typography>
+              </div>        
+            )}
+            {chartData && (
+                <LineChart width={500} height={250} data={chartData}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                <CartesianGrid strokeDasharray="3 3" />
+                {/* <XAxis dataKey="name" /> */}
+                <YAxis tickFormatter={formatYAxis} />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="engagement" stroke="#82ca9d" />
+                </LineChart>
+            )}
           </>
         );
 };
